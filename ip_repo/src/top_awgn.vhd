0404976fe6cc290cfa304d5d482dd04875bbd5bc
kodenkrypteraren <112951955+kodenkrypteraren@users.noreply.github.com>
2026-05-13 15:02:23 +0200

    AWGN ip repo fix
    AWGN ip repo fix

--- ip_repo/src/top_awgn.vhd
+++ ip_repo/src/top_awgn.vhd
@@ -5,6 +5,7 @@ entity top_awgn is
   port (
     clk          : in  std_logic;
     reset        : in  std_logic;
+    snr_sel      : in  std_logic_vector(1 downto 0);
     valid        : out std_logic;
     awgn_re_dbg  : out std_logic_vector(7 downto 0);
     awgn_im_dbg  : out std_logic_vector(7 downto 0)
@@ -12,13 +13,32 @@ entity top_awgn is
 end entity;
 
 architecture rtl of top_awgn is
-  signal ce_out     : std_logic;
-  signal awgn_re    : std_logic_vector(25 downto 0);
-  signal awgn_im    : std_logic_vector(25 downto 0);
+  signal ce_out      : std_logic;
+  signal awgn_re     : std_logic_vector(25 downto 0);
+  signal awgn_im     : std_logic_vector(25 downto 0);
 
-  signal clk_enable : std_logic := '1';
-  signal snrdB_const : std_logic_vector(15 downto 0) := x"0329"; -- 5 dB SNR
+  signal clk_enable  : std_logic := '1';
+  signal snrdB_const : std_logic_vector(15 downto 0);
+
 begin
+
+  process(snr_sel)
+  begin
+    case snr_sel is
+      when "00" =>
+        snrdB_const <= x"0329"; -- 5 dB SNR
+
+      when "01" =>
+        snrdB_const <= x"0640"; -- example: 10 dB SNR
+
+      when "10" =>
+        snrdB_const <= x"0960"; -- example: 15 dB SNR
+
+      when others =>
+        snrdB_const <= x"0C80"; -- example: 20 dB SNR
+    end case;
+  end process;
+
   u_awgn : entity work.AWGNGenerator
     port map (
       clk        => clk,
@@ -34,4 +54,5 @@ begin
   -- Export only LSBs for debug
   awgn_re_dbg <= awgn_re(7 downto 0);
   awgn_im_dbg <= awgn_im(7 downto 0);
+
 end architecture;
