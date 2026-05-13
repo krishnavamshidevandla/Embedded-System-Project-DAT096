b7ff22b7f087517537cb4b5e59bb668656319227
shirleyjohn-msj <mariashirleyjohn16@gmail.com>
2026-05-13 15:12:28 +0200

    AWGN_without_SNR_SEL

---
+++ ip_repo_1/src/top_awgn.vhd
@@ -0,0 +1,37 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity top_awgn is
+  port (
+    clk          : in  std_logic;
+    reset        : in  std_logic;
+    valid        : out std_logic;
+    awgn_re_dbg  : out std_logic_vector(25 downto 0);
+    awgn_im_dbg  : out std_logic_vector(25 downto 0)
+  );
+end entity;
+
+architecture rtl of top_awgn is
+  signal ce_out     : std_logic;
+  signal awgn_re    : std_logic_vector(25 downto 0);
+  signal awgn_im    : std_logic_vector(25 downto 0);
+
+  signal clk_enable : std_logic := '1';
+  signal snrdB_const : std_logic_vector(15 downto 0) := x"5000"; -- 5 dB SNR
+begin
+  u_awgn : entity work.AWGNGenerator
+    port map (
+      clk        => clk,
+      reset      => reset,
+      clk_enable => clk_enable,
+      snrdB      => snrdB_const,
+      ce_out     => ce_out,
+      awgn_re    => awgn_re,
+      awgn_im    => awgn_im,
+      valid      => valid
+    );
+
+  -- Export only LSBs for debug
+  awgn_re_dbg <= awgn_re;
+  awgn_im_dbg <= awgn_im;
+end architecture;
